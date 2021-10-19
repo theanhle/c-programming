ae6ffedb8b033a628eef62105667c9296b5a5b96
The Anh <theanhvmu@gmail.com>
2021-10-19 15:40:12 +0700

    update file linear search on parallel arrays

--- examples/linear search on parallel arrays.c
+++ examples/linear search on parallel arrays.c
@@ -1,10 +1,41 @@
-//  ProdId  Price   Quantity
-    //   12      125      1.5
-    //   45      152      2.6
-    //   57      144      3.4
-    
 #include <stdio.h>
-#define MAX_SIZE 100
+#define MAX_ITEMS 100
+
+/*
+    Product Id      Price     Quantity
+        12          100         3.5
+        34          250         4.5
+        17          120         2.8
+        89          180         4.3
+*/
+
+void get_items(int prod_id[], int price[], double quantity[], int *n)
+{
+    int i;
+    
+    printf("Enter number of items: ");
+    scanf("%d", n);
+    
+    for (i = 0; i < *n; i++)
+    {
+        printf("\nEnter item %d:\n", i+1);
+        printf(" + Product ID: ");
+        scanf("%d", &prod_id[i]);
+        printf(" + Price: ");
+        scanf("%d", &price[i]);
+        printf(" + Quantity: ");
+        scanf("%lf", &quantity[i]);
+    }
+}
+
+void print_items(int prod_id[], int price[], double quantity[], int n_items)
+{
+    int i;
+    printf("\nProduct Id   Price   Quantity\n");
+    printf("=============================\n");
+    for (i = 0; i < n_items; i++)
+        printf("%010d   %5d   %8.2lf\n", prod_id[i], price[i], quantity[i]);
+}
 
 int find(int search, int key[], int size)
 {
@@ -15,36 +46,23 @@ int find(int search, int key[], int size)
     return rc;
 }
 
-int main()
+int main ()
 {
-    int prod_id[MAX_SIZE], price[MAX_SIZE];
-    double quantity[MAX_SIZE];
-    int n_items, i, idx, max_price, pid;
+    int prod_id[MAX_ITEMS], price[MAX_ITEMS];
+    double quantity[MAX_ITEMS];
+    int n_items, pid, idx;
     
-    printf("Enter number of items: ");
-    scanf("%d", &n_items);
+    get_items(prod_id, price, quantity, &n_items);
+    print_items(prod_id, price, quantity, n_items);
     
-    for (i = 0; i < n_items; i++)
-    {
-        printf("Enter item %d:\n", i+1);
-        printf(" + Product ID: ");
-        scanf("%d", &prod_id[i]);
-        printf(" + Price: ");
-        scanf("%d", &price[i]);
-        printf(" + Quantity: ");
-        scanf("%lf", &quantity[i]);
-        printf("\n");
-    }
-    
-    printf("Enter Product ID: ");
+    printf("\nEnter ID of the product you want to find: ");
     scanf("%d", &pid);
     
     idx = find(pid, prod_id, n_items);
-    
     if (idx == -1)
         printf("No item found.");
     else
         printf("Product ID: %d, Price: %d, Quantity: %.2lf", prod_id[idx], price[idx], quantity[idx]);
-
+         
     return 0;
 }
