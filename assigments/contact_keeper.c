36f462aeae50d9ca2aac8deabe64277e3b694b67
The Anh <theanhvmu@gmail.com>
2021-12-31 22:54:48 +0700

    Add the assingment for Spring 2022

---
+++ assigments/contact_keeper.c
@@ -0,0 +1,38 @@
+/* So far, you've been learning the C programing language aobut two weeks.
+ * Now is the right time to think about your assignment.
+ * This semester, your task is to right a C program, namely Contact Keeper,
+ * to manage your contacts easily and effectively.
+ * 
+ * Suppose that one contact has below features:
+ *		1. Contact Name
+ *		2. Phone Number
+ *		3. Email
+ *		4. Working Address
+ *		5. Home Address
+ *		(feel free to add the others you like)
+ *
+ * Your program should have below functions:
+ *	1. List all contact in the table format
+ *	2. Add new contact
+ *	3. Edit contact
+ *	4. Search contact (by name or phone number)
+ *	5. Delete contact (by name or phone number)
+ *
+ *	The contact information should be stored permanently in a text file
+ *	so that when the program starts all contact could be loaded automatically.
+ *
+ *
+ *	This assignment can be done by a group of three students.
+ */
+
+#include <stdio.h>
+
+// prototype declaration
+
+
+int main()
+{
+	printf("CONTACT KEEPER");
+
+	return 0;
+}
