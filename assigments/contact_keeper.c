6f0e3b69da7ffab0728d53cd14586d15bb836db9
The Anh <theanhvmu@gmail.com>
2022-01-01 10:24:33 +0700

    assignment 2

--- assigments/contact_keeper.c
+++ assigments/contact_keeper.c
@@ -16,22 +16,22 @@
  *		6. Home Address
  *		7. Birthday
  *
- * Data validation is need to make sure the user enters the correct data. For example:
+ * Data validation should be implemented, for example:
  *		1. Phone Number should be a number of 9 or 10 digits
  *		2. Bithday should be in the DD/MM/YYYY format 
  *
  * Your program should have at least six functions:
- *	1. Add new contact
- *	2. Edit contact
- *	3. Delete contact (by name or phone number)
- *	4. Search contact (by name or phone number)
- *	5. List all contacts with birthdays in a given month (sort by date)
- *	6. List all contacts in the table format (sort by name)
+ *		1. Add new contact
+ *		2. Edit contact
+ *		3. Delete contact (by name or phone number)
+ *		4. Search contact (by name or phone number)
+ *		5. List all contacts with birthdays in a given month (sort by date)
+ *		6. List all contacts in the table format (sort by name)
  *
- *	The contact information should be permanently stored in a text file
- *	so that when the program starts all contacts could be loaded automatically.
+ * The contact information should be permanently stored in a text file
+ * so that when the program starts all contacts could be loaded automatically.
  *
- *	Form a group of three members and enjoy this task!
+ * Form a group of three members and enjoy this task!
  */
 
 #include <stdio.h>
