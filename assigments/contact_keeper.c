725deb2865548b77e65d6e77668e1a3e89ae6dad
The Anh <theanhvmu@gmail.com>
2022-01-01 10:17:46 +0700

    assignment 2

--- assigments/contact_keeper.c
+++ assigments/contact_keeper.c
@@ -1,28 +1,37 @@
-/* So far, you've been learning the C programing language aobut two weeks.
+/* CONTACT KEEPER
+ * ==============
+ * 
+ * So far, you've been learning the C programing language about two weeks.
  * Now is the right time to think about your assignment.
- * This semester, your task is to right a C program, namely Contact Keeper,
+ * This semester, your task is to write a C program, namely Contact Keeper,
  * to manage your contacts easily and effectively.
  * 
- * Suppose that one contact has below features:
- *		1. Contact Name
- *		2. Phone Number
- *		3. Email
- *		4. Working Address
- *		5. Home Address
- *		(feel free to add the others you like)
+ * One contact should have the following features:
+ *		1. First Name
+ *		2. Last Name
+ *		3. Company
+ *		3. Phone Number
+ *		4. Email
+ *		5. Working Address
+ *		6. Home Address
+ *		7. Birthday
  *
- * Your program should have below functions:
- *	1. List all contact in the table format
- *	2. Add new contact
- *	3. Edit contact
+ * Data validation is need to make sure the user enters the correct data. For example:
+ *		1. Phone Number should be a number of 9 or 10 digits
+ *		2. Bithday should be in the DD/MM/YYYY format 
+ *
+ * Your program should have at least six functions:
+ *	1. Add new contact
+ *	2. Edit contact
+ *	3. Delete contact (by name or phone number)
  *	4. Search contact (by name or phone number)
- *	5. Delete contact (by name or phone number)
+ *	5. List all contacts with birthdays in a given month (sort by date)
+ *	6. List all contacts in the table format (sort by name)
  *
- *	The contact information should be stored permanently in a text file
- *	so that when the program starts all contact could be loaded automatically.
+ *	The contact information should be permanently stored in a text file
+ *	so that when the program starts all contacts could be loaded automatically.
  *
- *
- *	This assignment can be done by a group of three students.
+ *	Form a group of three members and enjoy this task!
  */
 
 #include <stdio.h>
@@ -32,7 +41,9 @@
 
 int main()
 {
-	printf("CONTACT KEEPER");
+	printf("CONTACT KEEPER\n");
 
 	return 0;
 }
+
+// function definition
