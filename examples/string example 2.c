8464c07c194ab7cf5b85822ff0c30cdd7da9fbba
The Anh <theanhvmu@gmail.com>
2022-04-09 20:52:48 +0700

    example of sentence processing

--- examples/string example 2.c
+++ examples/string example 2.c
@@ -1,7 +1,8 @@
 /* Chuong trinh xu ly xau ki tu: */
 /* 1. Ki tu dau viet hoa, cac ki tu khac viet thuong */
 /* 2. Sau dau cham, dau phay la mot dau cach */
-/* 3. Giua cac tu la mot dau cach */
+/* 3. Dau cham, dau phay phai duoc viet sat vao ki tu lien truoc */
+/* 4. Giua cac tu la mot dau cach */
 
 #include <stdio.h>
 #include <string.h>
@@ -10,7 +11,7 @@
 
 int main()
 {
-	char s[100], t[100], c;
+	char s[200], t[200], c;
 	int i, n = 0;
 
 	scanf("%[^\n]", s);
@@ -21,21 +22,27 @@ int main()
 			if (n > 0 && t[n-1] != ' ')
 				t[n++] = ' ';
 		}
-		else
+		else if (s[i] == '.' || s[i] == ',')
 		{
-			if (n > 1 && t[n-1] == ' ' && t[n-2] == '.')
-				t[n++] = toupper(s[i]);
-			else if (n > 0 && (t[n-1] == '.' || t[n-1] == ','))
-			{
-				t[n++] = ' ';
-				if (t[n-2] == '.')
-					t[n++] = toupper(s[i]);
-				else
-					t[n++] = s[i];
-			}
+			if (t[n-1] == ' ')
+				t[n-1] = s[i];
 			else
-				t[n++] = tolower(s[i]);
+				t[n++] = s[i];
 		}
+		else if (n == 0)
+			t[n++] = toupper(s[i]);
+		else if (n > 1 && t[n-1] == ' ' && t[n-2] == '.')
+			t[n++] = toupper(s[i]);
+		else if (n > 0 && (t[n-1] == '.' || t[n-1] == ','))
+		{
+			t[n++] = ' ';
+			if (t[n-2] == '.')
+				t[n++] = toupper(s[i]);
+			else
+				t[n++] = s[i];
+		}
+		else
+			t[n++] = tolower(s[i]);
 
 	printf("%s", t);
 
